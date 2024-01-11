94a100af4404dc8c862fa9f91130a586a5018ee1
Viacheslav Onatskiy <slava9678@gmail.com>
2024-01-10 22:04:11 -0800

    Edit document.title depending on page

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,52 @@
+import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
-import Header from '../Header';
-import Footer from '../Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { PAGES } from '../../constants.js';
+import Footer from '../Footer';
+import Header from '../Header';
 import ScrollToTopButton from '../atoms/ScrollToTop/ScrollToTop.js';
 import { MainOutlet } from './styles.js';
-import { useEffect } from 'react';
 
 const Layout = () => {
   const location = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  useEffect(() => {
+    const defineDocumentTitle = (name = '') => {
+      document.title = `${name && name + ' | '}Yana Nails`;
+    };
+
+    switch (location.pathname) {
+      case PAGES.ABOUT:
+        defineDocumentTitle('About me');
+        break;
+      case PAGES.PORTFOLIO:
+        defineDocumentTitle('My Portfolio');
+        break;
+      case PAGES.REVIEWS:
+        defineDocumentTitle('Reviews');
+        break;
+      case PAGES.SERVICES:
+        defineDocumentTitle('Services');
+        break;
+      case PAGES.CONTACT:
+        defineDocumentTitle('My Contacts');
+        break;
+      case PAGES.LOGIN:
+        defineDocumentTitle('Login');
+        break;
+      case PAGES.REGISTER:
+        defineDocumentTitle('Register');
+        break;
+
+      default:
+        defineDocumentTitle();
+        break;
+    }
   }, [location.pathname]);
 
   return (
