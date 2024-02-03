e6a9d953867266916180669471340078b64f3874
Viacheslav Onatskiy <slava9678@gmail.com>
2024-02-03 15:09:46 -0800

    Edit some project descriptions and package dependencies

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [thunk],
+  // middleware: [thunk],
   preloadedState: initialState
 });
 
